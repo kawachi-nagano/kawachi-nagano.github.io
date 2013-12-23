44d607385e0911046398be5582c9230a4b73b188
kawachi　nagano <prajna0paramita@gmail.com>
2013-12-23 20:25:25 +0900

    Update setting.js

--- js/setting.js
+++ js/setting.js
@@ -10,7 +10,7 @@ Taxes.taxRate = 0.06; // 住民税率
 
 var OpenSpending = OpenSpending || {};
 
-OpenSpending.identifier = 'kawachinagano';
+OpenSpending.identifier = 'kawachinagano_2013';
 OpenSpending.year = '2013';
 
 OpenSpending.Styles = OpenSpending.Styles || {};
