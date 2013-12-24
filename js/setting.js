6e5615b8607b30e2523850d3585de724c9199141
kawachi　nagano <prajna0paramita@gmail.com>
2013-12-24 13:04:42 +0900

    Update setting.js

--- js/setting.js
+++ js/setting.js
@@ -18,8 +18,8 @@ OpenSpending.Styles = OpenSpending.Styles || {};
 OpenSpending.Styles.Cofog = {
 
   /* yokohama cofog */
-  '1': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },
-  '1-1': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },  
+  '1': { icon: 'icons/government-uk.svg', color: '#C75746', bcolor: '#935B3B' },
+  '1-1': { icon: 'icons/government-uk.svg', color: '#C75746', bcolor: '#935B3B' },  
   '2': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },  
   '2-1': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },  
   '2-2': { icon: 'icons/helping-others.svg', color: '#C75746', bcolor: '#935B3B' },  
